53456719ebf9e012d6ce4b3bf84e8b4c11e53438
denisanhel <tkachuk.anh@gmail.com>
2024-02-22 21:45:45 +0200

    Lection1.html  Lection1.js done

---
+++ HW-1/Lection1.js
@@ -0,0 +1,58 @@
+let one=('hello')
+let two=('owu')
+let three=('com')
+let four=('ua')
+let five=('1')
+let six=('10')
+let seven=('-999')
+let eight=('123')
+let nine=('3.14')
+let ten=('2.7')
+let eleven=('16')
+let twelve=('true')
+let thirteen=('false')
+
+
+
+
+
+
+console.log(one)
+console.log(two)
+console.log(three)
+console.log(four)
+console.log(five)
+console.log(six)
+console.log(seven)
+console.log(eight)
+console.log(nine)
+console.log(ten)
+console.log(eleven)
+console.log(twelve)
+console.log(thirteen)
+
+
+//--------------------------------------second-----------------------------------------------//
+
+
+
+let firstNAME = 'Denis'
+let middleName= 'Tkachuk-Angel'
+let lastName = 'Romanovych'
+
+console.log(firstNAME, middleName, lastName)
+
+
+
+//---------------------------------------third--------------------------------------------------//
+
+
+
+let a = 100;
+let b = '100';
+let c = true;
+
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
