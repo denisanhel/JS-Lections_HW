9bdd07a36f63fc4b8dd63f3e2ed0aaef6f92d443
denisanhel <tkachuk.anh@gmail.com>
2024-02-24 14:36:55 +0200

    Lection1.html  Lection1.js done

---
+++ HW-2/Lection2.js
@@ -0,0 +1,30 @@
+// ///Масиви та об'єкти:
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" -
+// являється  масивом.
+//     Кожен автор має поля name та age.
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password.' +
+// ' Вивести в консоль пароль кожного користувача
+
+
+let CatsZone = [
+
+    {name: 'Liza', id: 0, breed: 'persian', age: 1},
+    {name: 'Nafanya', id: 1, breed: 'sphinks', age: 2},
+    {name: 'Anya', id: 2, breed: 'persian', age: 4},
+    {name: 'Tanya', id: 3, breed: 'tiger', age: 5},
+    {name: 'Lusa', id: 4, breed: 'lynx', age: 1},
+    {name: 'ananaska', id: 5, breed: 'britain', age: 2},
+    {name: 'apelsinka', id: 6, breed: 'persian', age: 3},
+    {name: 'ryzyk', id: 7, breed: 'britain', age: 6},
+    {name: 'Lola', id: 8, breed: 'street', age: 1},
+    {name: 'Staff', id: 9, breed: 'street', age: 7},
+];
+
+
+console.log(CatsZone[0].name)
+console.log(CatsZone[0].breed)
+console.log(CatsZone[0].age)
+
+
