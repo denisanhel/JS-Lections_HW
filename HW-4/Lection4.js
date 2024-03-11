5d5223d5d54d5a935cbfa38dd0b0de9a9dae0b98
denisanhel <tkachuk.anh@gmail.com>
2024-03-11 18:40:49 +0200

    done Lection4.js

--- HW-4/Lection4.js
+++ HW-4/Lection4.js
@@ -91,3 +91,45 @@ function lastList(text, kilkist){
 }
 
 lastList('Random text', 3)
+
+
+// - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+
+let changes = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+
+function spusok(info){
+    document.write(`<ul>`)
+    for (const infoElement of info) {
+        document.write(`<li>${changes}</li>`)
+    }
+    document.write(`</ul>`)
+}
+
+spusok('changes')
+
+
+// - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+
+
+// - створити функцію яка повертає найменьше число з масиву
+
+
+// - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
+
+// - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
+// Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
+
+// - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
+// Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
