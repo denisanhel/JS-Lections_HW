9fa1773be123ff66e33a3cb7ba6649d0c6d986d6
denisanhel <tkachuk.anh@gmail.com>
2024-03-18 21:02:15 +0200

    done Lection4.js

--- HW-4/Lection4.js
+++ HW-4/Lection4.js
@@ -5,6 +5,7 @@
 function foo(a, b){
     return a * b;
 }
+
 let S = foo(6,90);
 console.log(S)
 
@@ -95,41 +96,116 @@ lastList('Random text', 3)
 
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
 
-let changes = [
-    {name: 'vasya', age: 31, status: false},
-    {name: 'petya', age: 30, status: true},
-    {name: 'kolya', age: 29, status: true},
-    {name: 'olya', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'anya', age: 31, status: false},
-    {name: 'oleg', age: 28, status: false},
-    {name: 'andrey', age: 29, status: true},
-    {name: 'masha', age: 30, status: true},
-    {name: 'olya', age: 31, status: false},
-    {name: 'max', age: 31, status: true}
-];
-
-function spusok(info){
+function accepter(array){
     document.write(`<ul>`)
-    for (const infoElement of info) {
-        document.write(`<li>${changes}</li>`)
+    for (const arrayElement of array) {
+        document.write(`<li>${array}</li>`)
     }
     document.write(`</ul>`)
 }
 
-spusok('changes')
+let array = ['hello', 12, 32, true, false]
+accepter(array)
+
+
 
 
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+function acceptArray(mas) {
+    for (let i = 0; i < mas.length; i++) {
+        let m = mas[i];
 
+        document.write(`<div>`);
+        document.write(`<p>Id: ${mas.id}</p>`);
+        document.write(`<p>Name: ${mas.name}</p>`);
+        document.write(`<p>Age: ${mas.age}</p>`);
+        document.write(`</div>`);
+    }
 
-// - створити функцію яка повертає найменьше число з масиву
+    return mas
+}
+
+let mas = [
+    { id: 1, name: 'anya', age: 30 },
+    { id: 2, name: 'kolya', age: 25 },
+    { id: 3, name: 'olya', age: 40 }
+];
+
+acceptArray(mas);
+
+// - створити функцію яка повертає найменше число з масиву
+
+function minNumber(mass){
+    let min = mass[0];
+    for (let i = 0; i < mass.length; i++) {
+        if(mass[i] < min) {
+            min = mass[i]
+        }
+    }
+return min
+
+}
+
+let numbers = [0, 1 ,2, 43, 2, 12, 32]
+console.log(minNumber(numbers))
+
 
 
 // - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
 
-// - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
+function sum(arr){
+let suma = 0;
+    for (let i = 0; i < arr.length; i++) {
+        suma + arr[i]
+    }
+    return suma
+}
+
+let chysla = [1, 2 ,4 ,5, 12 ,54, 12]
+console.log(sum(chysla))
+
+
+
+// - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відповідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
+
+
+function swap(arr, index1, index2) {
+
+    let first = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = first;
+
+    return arr;
+}
+
+let arrays = [1,2, 10, 22, 20, 30, 44];
+console.log(swap(arrays, 0, 1));
+
+
+
 
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
+
+function exchange(sumUAH, currencyValues, exchangeCurrency) {
+    let exchange;
+    for (let i = 0; i < currencyValues.length; i++) {
+        if (currencyValues[i].currency === exchangeCurrency) {
+            exchange = currencyValues[i].value;
+        }
+    }
+
+
+    let exchangedAmount = sumUAH / exchange;
+    return exchangedAmount;
+}
+
+let sumUAH = 10000;
+let currencyValues = [
+    { currency: 'USD', value: 20 },
+    { currency: 'EUR', value: 42 }
+];
+let exchangeCurrency = 'USD';
+
+console.log(exchange(sumUAH, currencyValues, exchangeCurrency));
